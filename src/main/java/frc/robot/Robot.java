26bc1da2eb6cb54f6304d8f6ac3d9c900e055029
Max <wmax16542@gmail.com>
2023-10-19 19:11:46 -0400

    init limelight in robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,12 @@
 
 package frc.robot;
 
+import org.opencv.imgproc.LineSegmentDetector;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Extensions.Limelight;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -16,6 +19,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
+  private Limelight limelight;
 
   private RobotContainer m_robotContainer;
 
@@ -28,6 +32,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    limelight = new Limelight();
   }
 
   /**
